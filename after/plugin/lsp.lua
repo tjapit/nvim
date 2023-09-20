96bb1b4e6f732477b15a45b6a8412091579441d2
Timothy Japit <timothy.japit@gmail.com>
2023-09-20 16:54:35 +0700

    fix (lsp-zero): new config for v3.x
    ensure_installed language servers within mason, setting up the language servers
    installed with lsp-zero, and cmp.setup for mappings (that for some
    reason doesn't work with .py files)

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -2,7 +2,25 @@ local lsp = require('lsp-zero')
 
 lsp.preset('recommended')
 
-lsp.ensure_installed({
+require('mason').setup({})
+require('mason-lspconfig').setup({
+  ensure_installed = {
+    'tsserver',
+    'eslint',
+    'rust_analyzer',
+    'lua_ls',
+    'pyright'
+  },
+  handlers = {
+    lsp.default_setup,
+    lua_ls = function()
+      local lua_opts = lsp.nvim_lua_ls()
+      require('lspconfig').lua_ls.setup(lua_opts)
+    end,
+  }
+})
+
+lsp.setup_servers({
     'tsserver',
     'eslint',
     'rust_analyzer',
@@ -24,17 +42,20 @@ lsp.configure('lua_ls', {
 
 local cmp = require('cmp')
 local cmp_select = { behavior = cmp.SelectBehavior.Select }
-local cmp_mappings = lsp.defaults.cmp_mappings({
+
+cmp.setup({
+  window = {
+    completion = cmp.config.window.bordered(),
+    documentation = cmp.config.window.bordered(),
+  },
+  mapping = cmp.mapping.preset.insert({
     ['<up>'] = cmp.mapping.select_prev_item(cmp_select),
     ['<down>'] = cmp.mapping.select_next_item(cmp_select),
     ['<C-u>'] = cmp.mapping.scroll_docs(-4),
     ['<C-d>'] = cmp.mapping.scroll_docs(4),
     ['<CR>'] = cmp.mapping.confirm({ select = true }),
     ['<C-Space>'] = cmp.mapping.complete(),
-})
-
-lsp.setup_nvim_cmp({
-    mapping = cmp_mappings
+  })
 })
 
 lsp.on_attach(function(client, bufnr)
@@ -45,6 +66,7 @@ lsp.on_attach(function(client, bufnr)
     vim.keymap.set("n", "]d", function() vim.diagnostic.goto_next() end, opts)
     vim.keymap.set("n", "[d", function() vim.diagnostic.goto_prev() end, opts)
 end)
+
 
 lsp.setup()
 
