6e6a38ee5525494bb3476986e23b2b36c8b944f0
Timothy Japit <timothy.japit@gmail.com>
2023-10-23 19:12:05 +0700

    feat (telescope): included hidden & gitignore files when calling find_files

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -4,3 +4,17 @@ vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>fs', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
+require('telescope').setup({
+  defaults = {
+    hidden = true,
+    file_ignore_patterns = {
+      "node_modules", "build", "dist", "yarn.lock"
+    }
+  },
+  pickers = {
+    find_files = {
+      hidden = true,
+      find_command = { "rg", "--files", "--hidden", "--no-ignore", "--glob", "!**/.git/*" }
+    }
+  }
+})
